d1df02cc5ac0b6f44b7a34f93502520e5ade69c9
Travis Cox <t.ward.cox@gmail.com>
2020-07-10 09:34:28 -0700

    Base App

---
+++ src/contact/Contact.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {makeStyles, Grid, Typography} from '@material-ui/core';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+
+  }
+})
+)
+
+const Contact = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item>
+        <Typography>
+          Contact
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Contact;
---
+++ src/home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {makeStyles, Grid, Typography} from '@material-ui/core';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.palette.primary.light,
+  }
+})
+)
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item>
+        <Typography className={classes.root}>
+          Home
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Home;
